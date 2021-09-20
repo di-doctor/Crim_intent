16ce98400d39c6d8e4775fd3e3cf9e9275fc29d8
Dima <di-doctor@yandex.ru>
2021-09-20 23:18:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/crim_intent/CrimeListFragment.kt
@@ -0,0 +1,42 @@
+package com.example.crim_intent
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment : Fragment() {
+
+    private val crimeListViewModel: CrimeListViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total Crimes :${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list,container,false)
+        val crimeRecycleView  = view.findViewById(R.id.crime_recycle_view) as RecyclerView
+        crimeRecycleView.layoutManager = LinearLayoutManager(context)
+        return view
+    }
+
+    companion object {
+        fun newInstance(): CrimeListFragment {
+            return CrimeListFragment()
+        }
+    }
+}
